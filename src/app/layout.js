d95f838d24dcc49d4269feb3d52a60e54b4e5653
Shehzad Murtaza <mr.shehzad457@gmail.com>
2025-02-24 23:11:46 +0500

    ok

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+'use client'
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Navbar from "@/components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" 
+        />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+       
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/metadata.js
@@ -0,0 +1,4 @@
+export const metadata = {
+  title: "SkillMatch - About Us",
+  description: "Learn about SkillMatch - A pioneering app specializing in assessment and screening solutions for businesses worldwide.",
+}; 
