735f201e8486360b01fa7ea770019967ef1acceb
Shehzad Murtaza <mr.shehzad457@gmail.com>
2025-02-24 18:43:26 +0500

    minor fixes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css"; 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
